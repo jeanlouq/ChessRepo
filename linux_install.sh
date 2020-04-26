fcd223fa2e569b0af31f22541a8702a1361f8185
jeanlouq <56448546+jeanlouq@users.noreply.github.com>
2020-04-26 08:57:53 +0200

    Libraries installation file for Linux

---
+++ linux_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install --user --upgrade pip
+pip install --user opencv-python
+pip install --user numpy
+pip install --user python-vlc
+read -p "Libraries installation completed : press any key to continue ..."
